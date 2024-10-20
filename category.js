24eb0bdb560dd073e7e0fa052e6d26f0bb9b7afa
DrNighteye <133571015+DrNighteye@users.noreply.github.com>
2024-10-20 19:18:19 +0200

    Update category.js

--- category.js
+++ category.js
@@ -1,37 +1,37 @@
 // Speichert die ausgewählte Kategorie im localStorage
 function selectCategory(category) {
-    let jsonFile;
+  let jsonFile;
 
-    switch (category) {
-        case 'Neurologie Teil 1':
-            jsonFile = 'questions.json';
-            break;
-        case 'Neurologie Teil 2':
-            jsonFile = 'neurologieTeil2.json'; // Neue JSON-Datei für Neurologie Teil 2
-            break;
-        case 'Neurologie Teil 3':
-            jsonFile = 'neurologieTeil3.json';
-            break;
-        case 'Muskeltonusstörungen':
-            jsonFile = 'muskeltonus.json';
-            break;
-        case 'Bewusstseinsstörungen':
-            jsonFile = 'bewusstseinsstoerungen.json';
-            break;
-        case 'Neuropsychologische Syndrome':
-            jsonFile = 'neuropsychologie.json';
-            break;
-        case 'Sensibilitätsstörungen':
-            jsonFile = 'sensibilitaetsstoerungen.json';
-            break;
-        default:
-            console.error("Kategorie nicht gefunden");
-            return;
-    }
+  switch (category) {
+    case 'Neurologie Teil 1':
+      jsonFile = 'questions.json';
+      break;
+    case 'Neurologie Teil 2':
+      jsonFile = 'neurologieTeil2.json'; // Neue JSON-Datei für Neurologie Teil 2
+      break;
+    case 'Neurologie Teil 3':
+      jsonFile = 'neurologieTeil3.json';
+      break;
+    case 'Orthopädie':
+      jsonFile = 'Orthopädie.json';
+      break;
+    case 'Bewusstseinsstörungen':
+      jsonFile = 'bewusstseinsstoerungen.json';
+      break;
+    case 'Neuropsychologische Syndrome':
+      jsonFile = 'neuropsychologie.json';
+      break;
+    case 'Sensibilitätsstörungen':
+      jsonFile = 'sensibilitaetsstoerungen.json';
+      break;
+    default:
+      console.error("Kategorie nicht gefunden");
+      return;
+  }
 
-    // Speichere die ausgewählte JSON-Datei im localStorage
-    localStorage.setItem('selectedJsonFile', jsonFile);
+  // Speichere die ausgewählte JSON-Datei im localStorage
+  localStorage.setItem('selectedJsonFile', jsonFile);
 
-    // Weiterleitung zur Spielseite
-    window.location.href = 'GAME.html';
+  // Weiterleitung zur Spielseite
+  window.location.href = 'game.html';
 }
